eccae66b15b22314cc4ac45aaf0f42995027aa8a
Dennis Ploeger <develop@dieploegers.de>
2021-06-08 08:45:08 +0200

    fix: Corrected collate

--- databases.tf
+++ databases.tf
@@ -1,7 +1,7 @@
 resource "azurerm_postgresql_database" "db" {
   for_each            = toset(var.database_suffixes)
   charset             = "UTF8"
-  collation           = "en-US.UTF8"
+  collation           = "en-US"
   name                = "${var.project}${var.stage}db${each.value}"
   resource_group_name = var.resource_group
   server_name         = azurerm_postgresql_server.server.name
