967fc0a126d097bf1667699ffb6a1f555e1e251b
Oliver Michels <oliver.michels@bechtle.com>
2024-02-01 10:38:40 +0100

    remove esoteric error situation

--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -10,10 +10,6 @@ terraform {
 }
 
 provider "azurerm" {
-  # https://github.com/hashicorp/terraform-provider-azurerm/issues/3358
-  # our SP cannot register surplus azure providers for like Media or AI or chat-bots
-  skip_provider_registration = true
-
   features {}
 }
 
